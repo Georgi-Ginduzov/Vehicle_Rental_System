0c878267b70e2ae5bcd2488c458b313bb09c0d12
Georgi <georgi.ginduzov17d@gmail.com>
2024-06-16 20:26:30 +0300

    Entity structure

---
+++ RentalSystem/RentalSystem/Entities/Customer.cs
@@ -0,0 +1,70 @@
+﻿using RentalSystem.Entities.Helpers;
+
+namespace RentalSystem.Entities
+{
+    public class Customer
+    {
+        private string? name;
+        public string? phoneNumber;
+        public string? email;
+
+        public Customer(string? name, string? phoneNumber, string? email)
+        {
+            CustomerId = Guid.NewGuid();
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public Guid CustomerId { get; private set; }
+        public string? Name 
+        {
+            get => name;
+            set
+            {
+                if (value != null)
+                {
+                    name = value;
+                }
+            }
+        }
+        public string? PhoneNumber 
+        { 
+            get => phoneNumber;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(PhoneNumber), " cannot be null");
+                }
+
+                if (!Validator.IsValidPhoneNumber(value))
+                {
+                    throw new ArgumentException("Phone number is invalid");
+                }
+
+                phoneNumber = value;
+            }
+        }
+        public string? Email 
+        {
+            get => email;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(Email), " cannot be null");
+                }
+
+                if (!Validator.IsValidEmail(value))
+                {
+                    throw new ArgumentException("Email is invalid");
+                }
+                email = value;                
+            }
+        }
+
+        
+
+    }
+}
---
+++ RentalSystem/RentalSystem/Entities/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿namespace RentalSystem.Entities
+{
+    public class Vehicle
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal RentalRate { get; set; } // Calculate it in the business logic
+        public bool IsAvailable { get; set; } // Make a set in the vehicles array
+
+
+        public Vehicle(string make, string model, decimal rentalRate)
+        {
+            Id = vehicleId;
+            Make = make;
+            Model = model;
+            RentalRate = rentalRate;
+            IsAvailable = true;
+        }
+
+        /*public void AddMaintenanceRecord(Maintenance maintenance)
+        {
+            MaintenanceRecords.Add(maintenance);
+        }*/
+    }
+}
